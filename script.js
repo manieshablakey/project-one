cf7bf363abb63d2fc24a728e10a630abac948101
manieshablakey <manieshab@gmail.com>
2018-09-19 15:15:07 +0800

    fixed bugs in basic game logic

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//16 cards game. Match 8 pais of cards. 1 player. Randomize card arrangement
-//Pick a card and flip it over. Pick a second card and flip it over. 
-//If cards match, match alert and cards stay up. Player gets one point
-//If no match, cardsface down.
-//game ends when 8 pairs up or cardsTurnedOver ===16.
 
-
+//Color Matching game. There are 16 cards (8 pairs of colors)
 //Create cards. Add cards to array.
 var cardsArray = [
 {color: "blue"},
@@ -25,26 +20,40 @@ var cardsArray = [
 {color: "grey"},
 ];
 
+//Create a duplicate of the cardsArray called cardsArrayCopy. Create an empty array to hold the shuffled cards. Pick a random card. Push random card into the shuffled array.
+//Remove card from the cardsArrayCopy.
+var shuffledArray = [];
+
+var shuffleCards = function() {
+	var cardsArrayCopy = cardsArray.slice(0);
+		for (i = 0; i < 16; i++) {
+			var randomCard = Math.floor(Math.random() * (cardsArrayCopy.length));
+			// console.log(randomCard);
+			shuffledArray.push(cardsArrayCopy[randomCard]);
+			cardsArrayCopy.splice(randomCard, 1);
+		}
+		console.log(shuffledArray);
+	}
+shuffleCards();
+
 //New array for cards flipped over.
 var cardsTurnedOver = []; 
 
-//Check for end of game.
+// //Check for end of game.
 var checkGameEnd = function() {
 	if (cardsTurnedOver.length === 16) {
 		alert("Congratulations! You've matched all the card colors correctly.");
-
-		for (i = 0; i < cardsTurnedOver.length; i++) {
-			cardsTurnedOver[i].style.backgroundColor = "#588c7e";
-			cardsTurnedOver[i].style.color = "#588c7e";
-			cardsTurnedOver[i].addEventListener("click", flipCard);
-		}
+		shuffledArray.length = 0;
+		shuffleCards();
+		var container = document.getElementById("container");
+		document.body.removeChild(container);
+		createCards();
 	}
 }
 
 //Check for matches.
 var checkMatch = function() {
 	var cardItems = cardsTurnedOver.length;
-	console.log(cardItems);
 
 	if ((cardItems % 2 === 0) && (cardItems !== 0)) {	
 		if ((cardsTurnedOver[cardItems - 2].textContent === cardsTurnedOver[cardItems - 1].textContent)) {
@@ -52,7 +61,7 @@ var checkMatch = function() {
 			checkGameEnd();
 		} else {
 			alert(`Sorry, try again.`);
-			console.log(cardsTurnedOver);
+			//console.log(cardsTurnedOver);
 			cardsTurnedOver[cardItems - 2].style.backgroundColor = "#588c7e";
 			cardsTurnedOver[cardItems - 2].style.color = "#588c7e";
 			cardsTurnedOver[cardItems - 2].addEventListener("click", flipCard);
@@ -60,8 +69,7 @@ var checkMatch = function() {
 			cardsTurnedOver[cardItems - 1].style.color = "#588c7e";
 			cardsTurnedOver[cardItems - 1].addEventListener("click", flipCard);
 			cardsTurnedOver.pop();
-			cardsTurnedOver.pop();
-			console.log(cardsTurnedOver);	
+			cardsTurnedOver.pop();	
 		}
 	}
 }	
@@ -69,9 +77,9 @@ var checkMatch = function() {
 //Flip card over
 var flipCard = function(event) {
 	var cardSelected = event.target;
-	console.log(cardSelected);
+	//console.log(cardSelected);
 	cardsTurnedOver.push(cardSelected);
-	var cardColors = cardsArray[cardSelected.id].color;
+	var cardColors = shuffledArray[cardSelected.id].color;
 	cardSelected.style.backgroundColor = cardColors;
 	cardSelected.style.color = "black";
 	cardSelected.removeEventListener("click", flipCard);
@@ -84,17 +92,25 @@ var createCards = function() {
 	var divName = div.setAttribute("id", "container");
 	document.body.appendChild(div);
 
-	for (i = 0; i <= 15; i++) {
+	for (i = 0; i < 16; i++) {
 		var card = document.createElement("div");
 		card.setAttribute("id", i);
 		card.classList.add("card");
-		var cardColorName = cardsArray[i].color
+		var cardColorName = shuffledArray[i].color;
 		card.textContent = cardColorName;
 		card.addEventListener("click", flipCard);
 		div.appendChild(card);		
 	}
 }
-console.log(createCards());
+createCards();
+
+
+
+
+
+
+
+
 
 
 
