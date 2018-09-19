99abdfedc3075252903025c99f12a065c03f7aa1
manieshablakey <manieshab@gmail.com>
2018-09-19 10:28:55 +0800

    basic game logic working and bugs fixed

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//6 card game. Match 6 pairs of colors (16 cards total). 2 players. Randomize card arrangement.(3 by 2 row)
-//Player 1- picks one card. flips it over. picks a second card. flips it over. 
-//Player 1- if card 1 === card 2, its a match (match alert). Keep cards facing up. Player 1 gets 1 point. Player 1 gets another turn.
-//Player 1 - if card 1 !== card 2 , no match (no match alert). Turn cards over. Turn goes to player 2.
+//16 cards game. Match 8 pais of cards. 1 player. Randomize card arrangement
+//Pick a card and flip it over. Pick a second card and flip it over. 
+//If cards match, match alert and cards stay up. Player gets one point
+//If no match, cardsface down.
+//game ends when 8 pairs up or cardsTurnedOver ===16.
 
-//Player 2 = if card 1 === card, its a match (match alert). Keep cards facing up. Player 2 gets 1 point. Player 2 gets another turn.
-//Plater 2- if card 1 !== card 2, no match (no match alert). Turn cards over. Turn goes to player 1.
-
-//Game ends when all cards a facing up (matches ==)
 
 //Create cards. Add cards to array.
 var cardsArray = [
@@ -14,26 +11,71 @@ var cardsArray = [
 {color: "blue"},
 {color: "red"},
 {color: "red"},
+{color: "green"},
+{color: "green"},
+{color: "yellow"},
+{color: "yellow"},
+{color: "purple"},
+{color: "purple"},
+{color: "orange"},
+{color: "orange"},
+{color: "pink"},
+{color: "pink"},
+{color: "grey"},
+{color: "grey"},
 ];
 
-var turn;
+//New array for cards flipped over.
 var cardsTurnedOver = []; 
+
+//Check for end of game.
+var checkGameEnd = function() {
+	if (cardsTurnedOver.length === 16) {
+		alert("Congratulations! You've matched all the card colors correctly.");
+
+		for (i = 0; i < cardsTurnedOver.length; i++) {
+			cardsTurnedOver[i].style.backgroundColor = "#588c7e";
+			cardsTurnedOver[i].style.color = "#588c7e";
+			cardsTurnedOver[i].addEventListener("click", flipCard);
+		}
+	}
+}
+
+//Check for matches.
+var checkMatch = function() {
+	var cardItems = cardsTurnedOver.length;
+	console.log(cardItems);
+
+	if ((cardItems % 2 === 0) && (cardItems !== 0)) {	
+		if ((cardsTurnedOver[cardItems - 2].textContent === cardsTurnedOver[cardItems - 1].textContent)) {
+			alert(`You've matched the ${(cardsTurnedOver[cardItems - 1].textContent)} cards.`);	
+			checkGameEnd();
+		} else {
+			alert(`Sorry, try again.`);
+			console.log(cardsTurnedOver);
+			cardsTurnedOver[cardItems - 2].style.backgroundColor = "#588c7e";
+			cardsTurnedOver[cardItems - 2].style.color = "#588c7e";
+			cardsTurnedOver[cardItems - 2].addEventListener("click", flipCard);
+			cardsTurnedOver[cardItems - 1].style.backgroundColor = "#588c7e";
+			cardsTurnedOver[cardItems - 1].style.color = "#588c7e";
+			cardsTurnedOver[cardItems - 1].addEventListener("click", flipCard);
+			cardsTurnedOver.pop();
+			cardsTurnedOver.pop();
+			console.log(cardsTurnedOver);	
+		}
+	}
+}	
 
 //Flip card over
 var flipCard = function(event) {
 	var cardSelected = event.target;
-	var cardSelectedId = parseInt(event.target.id);
-	var cardColors = cardsArray[cardSelectedId].color;
+	console.log(cardSelected);
+	cardsTurnedOver.push(cardSelected);
+	var cardColors = cardsArray[cardSelected.id].color;
 	cardSelected.style.backgroundColor = cardColors;
-	cardsTurnedOver.push(cardsArray[cardSelectedId].color);
-	//console.log("This is cardsTurnedOVer array: " + cardsTurnedOver);
-		if (cardsTurnedOver.length === 2) {
-			if (cardsTurnedOver[0] === cardsTurnedOver[1]) {
-			alert("You found a match!");
-			} else {
-			alert("Sorry, try again.")
-			}
-		}
+	cardSelected.style.color = "black";
+	cardSelected.removeEventListener("click", flipCard);
+	checkMatch();
 }
 
 //Create a board of cards to play.
@@ -41,22 +83,24 @@ var createCards = function() {
 	var div = document.createElement("div");
 	var divName = div.setAttribute("id", "container");
 	document.body.appendChild(div);
-	for (i = 0; i <= 3; i++) {
+
+	for (i = 0; i <= 15; i++) {
 		var card = document.createElement("div");
 		card.setAttribute("id", i);
 		card.classList.add("card");
+		var cardColorName = cardsArray[i].color
+		card.textContent = cardColorName;
 		card.addEventListener("click", flipCard);
 		div.appendChild(card);		
 	}
 }
-createCards();
+console.log(createCards());
 
 
 
 
 
-
-
+		
 
 
 
